0088a8aea872c4ac340fb3c68ecd87d40a42c053
Cyber MacGeddon <cybermaggedon@gmail.com>
2020-07-19 17:23:42 +0100

    UI 0.3

--- ui.js
+++ ui.js
@@ -37,7 +37,7 @@ exports.resources = function(config, provider) {
                         containers: [
                             {
                                 name: "ui",
-                                image: "docker.io/cybermaggedon/evs-web:0.2",
+                                image: "docker.io/cybermaggedon/evs-web:0.3",
                                 ports: [
                                     {
                                         containerPort: 8080,
