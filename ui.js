b5d5a87d619a567e6e2bb515e1f6c2a1ffcebba1
Cyber MacGeddon <cybermaggedon@gmail.com>
2020-08-07 16:35:15 +0100

    UI to 0.7, includes enhanced event views.

--- ui.js
+++ ui.js
@@ -37,7 +37,7 @@ exports.resources = function(config, provider) {
                         containers: [
                             {
                                 name: "ui",
-                                image: "docker.io/cybermaggedon/evs-web:0.6",
+                                image: "docker.io/cybermaggedon/evs-web:0.7",
                                 ports: [
                                     {
                                         containerPort: 8080,
